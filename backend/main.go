eb766a220a0134c5a1d1add5cd8f572ed50cb088
SpiritFoxo <37273399+SpiritFoxo@users.noreply.github.com>
2025-03-18 15:21:26 +0300

    base files and docker setup

---
+++ backend/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"time"
+
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+)
+
+func SetupRouter() *gin.Engine {
+	r := gin.Default()
+
+	r.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"*"},
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"},
+		AllowHeaders:     []string{"Content-Type", "Authorization"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}))
+	return r
+
+}
+
+func main() {
+	r := SetupRouter()
+
+	r.Run(":8080")
+}
